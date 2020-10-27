b3937bc76221b4e9d8a187e65ce9e3daad899eb0
Faqih Muntashir <itsfaqih@gmail.com>
2020-10-27 11:18:46 +0700

    Added project

---
+++ auth/AuthConfig.ts
@@ -0,0 +1,10 @@
+export const AuthConfig = {
+  appId: '9bb4f79b-376d-453a-8486-de16063771de',
+  appScopes: [
+    'openid',
+    'offline_access',
+    'profile',
+    'User.Read',
+    'Calendars.Read'
+  ]
+};
